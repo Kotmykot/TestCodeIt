724be6949e67501a3b5836ff17d1d9ed245583a7
Nikita Kotenko <nork2014@gmail.com>
2017-10-28 21:18:51 +0300

    add an exception for db

--- DB.php
+++ DB.php
@@ -2,9 +2,15 @@
 class DB
 {
     public static function getConnection(){
+
+        try {
         $dbn = "mysql:host=localhost;dbname=task;charset=utf8";
         $db = new PDO($dbn,'root','');
         return $db;
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
     }
 }
 
